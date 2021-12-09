d59a9f4985c25539478d91e6913e79badfcbabc1
Ahmed Belal <ahmbelal95@gmail.com>
2021-12-09 10:28:24 +0200

    base templating

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -11,6 +12,31 @@ def home_page():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+
+def write_to_file(data):
+    with open("database.txt", "a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        database.write(f"\n{email}, {subject}, {message}")
+
+
+def write_to_csv_file(data):
+    with open("database.csv", "a", newline="") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(
+            database, delimiter=",", quoting=csv.QUOTE_MINIMAL
+        )
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route("/submit-form", methods=["GET", "POST"])
+def submit_form():
+    write_to_csv_file(request.form.to_dict())
+    return redirect("thankyou.html")
 
 
 if __name__ == "__main__":
